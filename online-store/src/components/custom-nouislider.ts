cef87d1461e8b8b698b1dbeefdb9373ed581ae60
NikitaKakurin <neyasut87@rambler.ru>
2022-07-13 15:17:21 +0300

    feat: add custom-nouislider.ts

---
+++ online-store/src/components/custom-nouislider.ts
@@ -0,0 +1,66 @@
+import { TargetElement, Formatter } from '../typescript/nouislider';
+import noUiSlider from 'nouislider';
+
+class CustomNouislider {
+    id: string;
+    min: number;
+    max: number;
+    tooltipsIdPart: string;
+    formatSlider: TargetElement;
+    constructor(id: string, min: number, max: number, tooltipsIdPart: string) {
+        this.id = id;
+        this.min = min;
+        this.max = max;
+        this.formatSlider = document.getElementById(this.id) as TargetElement;
+        this.tooltipsIdPart = tooltipsIdPart;
+        this.initSlider();
+    }
+
+    initSlider = (): void => {
+        if (this.formatSlider === null) throw new Error('no format slider');
+
+        const formatForSlider: Formatter = {
+            from: function (formattedValue: string) {
+                return Math.round(Number(formattedValue));
+            },
+            to: function (numericValue: number) {
+                return Math.round(numericValue);
+            },
+        };
+        noUiSlider.create(this.formatSlider, {
+            start: ['20.0', '80.0'],
+            connect: true,
+            range: {
+                min: this.min,
+                max: this.max,
+            },
+            step: 1,
+            format: formatForSlider,
+        });
+
+        if (this.formatSlider.noUiSlider === undefined) throw new Error('formatSlider.noUiSlider - undefined');
+
+        this.set(this.min, this.max);
+
+        const startValues = document.getElementById(`filter__${this.tooltipsIdPart}_start`);
+        const endValues = document.getElementById(`filter__${this.tooltipsIdPart}_end`);
+        if (!startValues || !endValues) throw new Error('startValues or endValues is null');
+        const formatValues = [startValues, endValues];
+
+        this.formatSlider.noUiSlider.on('update', function (values, handle) {
+            formatValues[handle].innerHTML = values[handle].toString();
+        });
+    };
+
+    set(min: number, max: number): void {
+        if (this.formatSlider.noUiSlider === undefined) throw new Error('formatSlider.noUiSlider - undefined');
+        this.formatSlider.noUiSlider.set([min, max]);
+    }
+
+    getValues() {
+        if (this.formatSlider.noUiSlider === undefined) throw new Error('formatSlider.noUiSlider - undefined');
+        return this.formatSlider.noUiSlider.get();
+    }
+}
+
+export default CustomNouislider;
--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -1,38 +1,5 @@
 import './style/style.scss';
-import { TargetElement, Formatter } from './typescript/nouislider';
-import noUiSlider from 'nouislider';
+import CustomNouislider from './components/custom-nouislider';
 
-const formatSlider = document.getElementById('filter__age') as TargetElement;
-if (formatSlider == null) throw new Error('no format slider');
-
-const formatForSlider: Formatter = {
-    from: function (formattedValue: string) {
-        return Math.round(Number(formattedValue));
-    },
-    to: function (numericValue: number) {
-        return Math.round(numericValue);
-    },
-};
-noUiSlider.create(formatSlider, {
-    start: ['20.0', '80.0'],
-    connect: true,
-    range: {
-        min: 1980,
-        max: 2022,
-    },
-    step: 1,
-    format: formatForSlider,
-});
-
-if (formatSlider.noUiSlider === undefined) throw new Error('formatSlider.noUiSlider - undefined');
-
-formatSlider.noUiSlider.set(['2000', '2021']);
-
-const startValues = document.getElementById('filter__age_start');
-const endValues = document.getElementById('filter__age_end');
-if (!startValues || !endValues) throw new Error('startValues or endValues is null');
-const formatValues = [startValues, endValues];
-
-formatSlider.noUiSlider.on('update', function (values, handle) {
-    formatValues[handle].innerHTML = values[handle].toString();
-});
+const ageSlider = new CustomNouislider('filter__age', 1980, 2022, 'age');
+const amountSlider = new CustomNouislider('filter__amount', 0, 100, 'amount');
